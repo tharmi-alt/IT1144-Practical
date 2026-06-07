3c0603d04e6fd272c4b21df1dbf6dda39dd83d1a
tharmi-alt <tharmitharmi157@gmail.com>
2026-06-07 20:11:15 +0530

    Add files via upload

---
+++ Day 9/If elseif.js
@@ -0,0 +1,16 @@
+let mark=35;
+if(mark>75){
+	console.log("A");	
+}
+else if(mark>65){
+	console.log("B");	
+}
+else if(mark>55){
+	console.log("C");	
+}
+else if(mark>35){
+	console.log("S");	
+}
+else {
+	console.log("F")
+}
